c342c376c38db607459c2740cb19ef006b063545
Shaedil <shaedildider@gmail.com>
2021-02-19 13:11:33 -0500

    chore: format spaces and functions

--- dice_roll/dice_roll.ino
+++ dice_roll/dice_roll.ino
@@ -13,8 +13,6 @@ By Shaedil Dider
 GPLv3.0 License
 */
 
-#define DEBUG 0
-
 // 6 consecutive digital pins for the LEDs
 int first = 2;
 int second = 3;
@@ -28,67 +26,66 @@ int button = 12;
 int pressed = 0;
 
 void setup() {
-        for (int i=first; i<=sixth; i++) {
-                pinMode(i, OUTPUT);
-        }
-        pinMode(button, INPUT);
-        // initialize random seed by noise from analog pin 0 (should be unconnected)
-        randomSeed(analogRead(0));
+    for (int i = first; i <= sixth; i++) {
+        pinMode(i, OUTPUT);
+    }
+    pinMode(button, INPUT);
+    // initialize random seed by noise from analog pin 0 (should be unconnected)
+    randomSeed(analogRead(0));
+}
 
-#ifdef DEBUG
-        Serial.begin(9600);
-#endif
-
+void loop() {
+    // if button is pressed - throw the dice
+    pressed = digitalRead(button);
+    if (pressed == HIGH) {
+        // remove previous number
+        setAllLEDs(LOW);
+        buildUpTension();
+        int thrownNumber = throwDice();
+        showNumber(thrownNumber);
+    }
 }
 
 void lightHalfOfLEDS(int first, int second, int third) {
-        digitalWrite(first, HIGH);
-        digitalWrite(second, HIGH);
-        digitalWrite(third, HIGH);
-        delay(200);
-        digitalWrite(first, LOW);
-        digitalWrite(second, LOW);
-        digitalWrite(third, LOW);
-        delay(200);
+    digitalWrite(first, HIGH);
+    digitalWrite(second, HIGH);
+    digitalWrite(third, HIGH);
+    delay(200);
+    digitalWrite(first, LOW);
+    digitalWrite(second, LOW);
+    digitalWrite(third, LOW);
+    delay(200);
 }
 
 void buildUpTension() {
-        lightHalfOfLEDS(3, 5, 7);
-        lightHalfOfLEDS(2, 4, 6);
-        lightHalfOfLEDS(3, 5, 7);
+    lightHalfOfLEDS(3, 5, 7);
+    lightHalfOfLEDS(2, 4, 6);
+    lightHalfOfLEDS(3, 5, 7);
 }
+
 void showNumber(int number) {
-        digitalWrite(first, HIGH);
-        if (number >= 2) { digitalWrite(second, HIGH); }
-        if (number >= 3) { digitalWrite(third, HIGH); }
-        if (number >= 4) { digitalWrite(fourth, HIGH); }
-        if (number >= 5) { digitalWrite(fifth, HIGH); }
-        if (number == 6) { digitalWrite(sixth, HIGH); }
+    digitalWrite(first, HIGH);
+    if (number >= 2) {
+        digitalWrite(second, HIGH);
+    } if (number >= 3) {
+        digitalWrite(third, HIGH);
+    } if (number >= 4) {
+        digitalWrite(fourth, HIGH);
+    } if (number >= 5) {
+        digitalWrite(fifth, HIGH);
+    } if (number == 6) {
+        digitalWrite(sixth, HIGH);
+    }
 }
+
 int throwDice() {
-        // get a random number in the range [1,6]
-        int randNumber = random(1,7);
-
-#ifdef DEBUG
-        Serial.println(randNumber);
-#endif
-
-        return randNumber;
+    // get a random number in the range [1,6]
+    int randNumber = random(1, 7);
+    return randNumber;
 }
 
 void setAllLEDs(int value) {
-        for (int i=first; i<=sixth; i++) {
-                digitalWrite(i, value);
-        }
-}
-void loop() {
-        // if button is pressed - throw the dice
-        pressed = digitalRead(button);
-        if (pressed == HIGH) {
-                // remove previous number
-                setAllLEDs(LOW);
-                buildUpTension();
-                int thrownNumber = throwDice();
-                showNumber(thrownNumber);
-        }
+    for (int i = first; i <= sixth; i++) {
+        digitalWrite(i, value);
+    }
 }
